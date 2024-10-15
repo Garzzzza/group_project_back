013fcc57a746a981ac9ae1dbd62e6461d8bfade6
Your Name <agerzee@gmail.com>
2024-10-15 21:04:12 +0300

    vercelJson

--- index.js
+++ index.js
@@ -9,13 +9,7 @@ const usersRouter = require("./routes/usersRoute");
 const scoresRouter = require("./routes/scoresRoute");
 const kGameRouter = require("./routes/kGameRoute");
 
-app.use(
-  cors({
-    origin: "https://game-app-client-seven.vercel.app", // Specify your frontend URL
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers if needed
-  })
-);
+app.use(cors());
 
 connectDB();
 
